07ae941294845f775cc4c053795d1ea7351d5cde
kanny <kanny.ghafour87@gmail.com>
2023-06-11 19:41:29 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const element = document.createElement("div");
+document.body.append(element);
+const newHeader = document.createElement("h1");
+newHeader.textContent = "Kanny is the champion";
+newHeader.id = 'victory';
+element.appendChild(newHeader);
